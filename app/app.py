248d75f044015c689f65c1829992d56e6deb7462
khem Sharma <khem@coloborarlabs.com>
2023-03-28 14:38:50 +0530

    Modifying cors

--- app/app.py
+++ app/app.py
@@ -4,9 +4,23 @@ from sqlalchemy.orm import Session
 from . import models
 from .models import Addresses
 from .schemas import Address
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1",
+    "http://127.0.0.1:8082",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 models.Base.metadata.create_all(bind=engine) # Create tables
 
 def get_db():
