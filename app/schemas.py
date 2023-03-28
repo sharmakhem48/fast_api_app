842d6b898ffae07d0fff8fd39f1894790a95d6bc
khem Sharma <khem@coloborarlabs.com>
2023-03-28 15:51:43 +0530

    Resolving errors

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 class Address(BaseModel):
-    street: str = Field(min_length=1)
-    city: str = Field(min_length=1)
-    state: str = Field(min_length=1)
-    country: str =  Field(min_length=1)
-    zip: str =  Field(min_length=1)
+    street: str
+    city: str
+    state: str
+    country: str
+    zip: str
     latitude: float
     longitude: float
